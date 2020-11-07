46c427fcccb25143160abcee1f824b3c662f72c9
Benbop9 <34316758+Benbop9@users.noreply.github.com>
2020-11-07 17:17:49 -0500

    very serious work

--- api/api.py
+++ api/api.py
@@ -3,7 +3,22 @@ import urllib.request
 from flask_cors import CORS
 from flask import Flask, request
 
-import db
+
+class user:
+    learn = []
+    teach = []
+    skillLearnCount = 0
+    skillTeachCount = 0
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+
+activeIndex = 0
+userCount = 0
+userList = []
+
 app = Flask(__name__)
 
 emails = ["Ben", "GeordyIsABitch", "Yi Li"]
@@ -11,7 +26,7 @@ passwords = ["fkGeordy", "same", "theCarry"]
 
 
 @app.route('/loginData', methods=['GET', 'POST'])
-def login():
+def loginData():
     data = request.get_json()
     email = data["email"]
     password = data["password"]
@@ -24,18 +39,28 @@ def login():
 
 
 @app.route('/registerData', methods=['GET', 'POST'])
-def register():
+def registerData():
+    global userCount
     data = request.get_json()
     email = data["email"]
     password = data["password"]
     if(email in emails):
+        return{'auth': str(0)}
+    else:
+        userList.append(user(email, password))
+        userCount += 1
+        print(userList[0].email)
+
         return{'auth': str(1)}
 
 
-@app.route('/getUser', methods=['GET', 'POST'])
-def register():
+@app.route('/userDataLearn', methods=['GET', 'POST'])
+def userDataLearn():
+    global activeIndex
     data = request.get_json()
-    email = data["email"]
-    password = data["password"]
-    if(email in emails):
-        return{'auth': str(1)}
+    skill = data["skill"]
+    level = data["level"]
+    userList.learn[userList[activeIndex].skillLearnCount].append([
+        skill, level])
+    userList[activeIndex].skillLearnCount += 1
+    return{}
