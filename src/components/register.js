7e70493e4565e663cbebe5136f8d8bc0cbaf3537
yili288 <47297703+yili288@users.noreply.github.com>
2020-11-08 10:08:47 -0500

    Update register.js

--- src/components/register.js
+++ src/components/register.js
@@ -40,7 +40,7 @@ function Register() {
         <Fragment>
             <div className="homepageDiv">
                 <div className="form">
-                    <h1>SkillShare</h1>
+                    <h1>Share</h1>
                     <h2>Email</h2>
                     <div className="inputDiv">
                         <input className="input" onChange={(e) => onChangeEmail(e)}></input>
